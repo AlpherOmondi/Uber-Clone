5195bcd106d09cb4fd5af3e1d0c4e8b850dec5f6
AlpherOmondi <alphaomondi001@gmail.com>
2023-09-30 14:00:27 +0300

    Feat: Added firebase login logic

--- app/src/main/java/com/example/uberclone/SplashScreenActivity.kt
+++ app/src/main/java/com/example/uberclone/SplashScreenActivity.kt
@@ -1,8 +1,12 @@
 package com.example.uberclone
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
@@ -26,11 +30,18 @@ class SplashScreenActivity : AppCompatActivity() {
 
     //Auth state listener
     private lateinit var _listener: AuthStateListener
+    // Getting results
+    private lateinit var getResult: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+    if(result.resultCode == Activity.RESULT_OK){
 
+    }
 
+}
+        init()
     }
 
     public override fun onStart() {
@@ -41,10 +52,11 @@ class SplashScreenActivity : AppCompatActivity() {
     private fun displaySplashScreen() {
         //Adding Toast to show that spalashscreen is working
         Completable.timer(3, TimeUnit.SECONDS, AndroidSchedulers.mainThread()).subscribe() {
-            Toast.makeText(this@SplashScreenActivity, "Welcome to uber...", Toast.LENGTH_LONG)
-                .show()
+
+            firebaseauth.addAuthStateListener(_listener)
 
         }
+
     }
 
     public override fun onStop() {
@@ -55,25 +67,34 @@ class SplashScreenActivity : AppCompatActivity() {
     }
 
     private fun init() {
-    providers = Arrays.asList(
-        AuthUI.IdpConfig.PhoneBuilder().build(),
-        AuthUI.IdpConfig.GoogleBuilder().build()
-    )
+        providers = Arrays.asList(
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
         firebaseauth = getInstance()
         _listener = FirebaseAuth.AuthStateListener { firebaseAuth ->
             val user = firebaseAuth.currentUser
-            if(user != null){
-                Toast.makeText(this,"Welcome ${user.uid}", Toast.LENGTH_LONG).show()}
-            else{
+            if (user != null) {
+                Toast.makeText(this, "Welcome ${user.uid}", Toast.LENGTH_LONG).show()
+            } else {
                 showLoginLayout()
             }
-            }
         }
+    }
 
     private fun showLoginLayout() {
-        val authMethodPickerLayout = AuthMethodPickerLayout(R.layout.)
+        val authMethodPickerLayout = AuthMethodPickerLayout.Builder(R.layout.sign_in_ui)
+            .setPhoneButtonId(R.id.button_phone_sign_in)
+            .setGoogleButtonId(R.id.button_google_sign_in)
+            .build()
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAuthMethodPickerLayout(authMethodPickerLayout)
+            .setAvailableProviders(providers)
+            .setIsSmartLockEnabled(false)
+            .build()
+        getResult.launch(signInIntent)
     }
 
 
-}
 }
